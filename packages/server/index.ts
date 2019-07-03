e81365936cd24d4ba595ebf49da089d1e43f470b
Brandon Konkle <brandon@konkle.us>
2019-07-03 15:42:02 -0600

    Add KnexUtils

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,3 +1,5 @@
 import {createRequest, query, mutation} from './src/utils/PostGraphileUtils'
+import * as PostGraphileUtils from './src/utils/PostGraphileUtils'
+import * as KnexUtils from './src/utils/KnexUtils'
 
-export {createRequest, query, mutation}
+export {PostGraphileUtils, KnexUtils, createRequest, query, mutation}
